5d88b98785abe0f1da7ac8bf1a206e26c1061a74
CapitalistFerrari <45477189+CapitalistFerrari@users.noreply.github.com>
2019-01-16 12:20:09 -0500

    Add files via upload

---
+++ GITHUBPUSHRH/GITHUBPUSHRH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITHUBPUSHRH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("t\Hello World");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
